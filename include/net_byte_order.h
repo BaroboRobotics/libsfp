6e0b56844677434eaa92ec071540d59bf42c0a03
David Ko <david.dk.ko@gmail.com>
2014-01-06 14:51:06 -0800

    Some modifications to build with Ch

--- include/net_byte_order.h
+++ include/net_byte_order.h
@@ -1,7 +1,14 @@
 #ifndef _NET_BYTE_ORDER_H_
 #define _NET_BYTE_ORDER_H_
 
+#ifdef _MSC_VER
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+#else
 #include <stdint.h>
+#endif
 
 uint16_t netByteOrder16 (uint16_t);
 uint32_t netByteOrder32 (uint32_t);
