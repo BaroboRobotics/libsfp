46bd8fc4db1abdaacc1784f0745c595dd1d8b750
Harris Hancock <vortrab@gmail.com>
2014-08-06 17:49:26 -0700

    Write SimulatedMedium

---
+++ tests/serial_framing_protocol.cpp
@@ -0,0 +1,99 @@
+//#define SFP_CONFIG_HISTORY_BUFFER_CAPACITY SFP_SEQ_RANGE
+
+//#include <sfp/sfp.hpp>
+
+#include "simulatedmedium.hpp"
+
+#include <chrono>
+#include <iostream>
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+#define PROPAGATION_DELAY_IN_BYTES 50
+#define SPEED_IN_MILLISECONDS_PER_BYTE 1
+#define CHANCE_OF_BIT_FLIP 0.0
+#define CHANCE_OF_BYTE_DROP 0.0
+
+uint8_t garble (uint8_t octet) {
+  for (int i = 0; i < 8; i++) {
+    if ((double)rand() / (double)RAND_MAX < CHANCE_OF_BIT_FLIP) {
+      octet ^= (1 << i);
+    }
+  }
+
+  return octet;
+}
+
+#if 0
+void alice_write (uint8_t octet, void *data) {
+#if 1
+  if ((double)rand() / (double)RAND_MAX < CHANCE_OF_BYTE_DROP) {
+    return;
+  }
+#endif
+  RINGBUF_PUSH_BACK(aliceToBob, garble(octet));
+}
+#endif
+
+int main () {
+    using Clock = std::chrono::system_clock;
+
+    srand(time(NULL));
+
+    SimulatedMedium medium { SimulatedMedium::Milliseconds(100), 50000 };
+
+    std::string hello { "Hello!" };
+    auto start = Clock::now();
+
+    medium.write(hello);
+    for (int i = 0; i < hello.size(); ++i) {
+        auto c = medium.read();
+        std::cout << c;
+    }
+
+    auto stop = Clock::now();
+    auto elapsedMs = std::chrono::duration_cast<SimulatedMedium::Milliseconds>(stop - start);
+    std::cout << "\nElapsed milliseconds: " << elapsedMs.count() << std::endl;
+
+#if 0
+    sfpInit(&alice);
+    sfpSetDeliverCallback(&alice, alice_deliver, NULL);
+    sfpSetWriteCallback(&alice, SFP_WRITE_ONE, alice_write, NULL);
+
+    sfpInit(&bob);
+    sfpSetDeliverCallback(&bob, bob_deliver, NULL);
+    sfpSetWriteCallback(&bob, SFP_WRITE_MULTIPLE, bob_write, NULL);
+
+#ifdef SFP_DEBUG
+    sfpSetDebugName(&alice, "alice");
+    sfpSetDebugName(&bob, "bob");
+#endif
+
+    sfpConnect(&alice);
+
+    for (int i = 0; i < 20; i++) {
+    do {
+      alice_write('\x7e', NULL);
+      bob_write("\x7e", 1, NULL);
+      serviceMedium(&aliceToBob, &bob);
+      serviceMedium(&bobToAlice, &alice);
+    } while (!sfpIsConnected(&alice) || !sfpIsConnected(&bob));
+
+    SFPpacket packet;
+
+    packet.len = snprintf((char *)packet.buf, SFP_CONFIG_MAX_PACKET_SIZE, "Hi Bob! (%d)", i);
+    sfpWritePacket(&alice, &packet);
+
+#if 1
+    packet.len = snprintf((char *)packet.buf, SFP_CONFIG_MAX_PACKET_SIZE, "Shut up Alice! (%d)", i);
+    sfpWritePacket(&bob, &packet);
+#endif
+
+    if (13 == i) {
+      printf("!!!! ALICE RECONNECTING !!!!\n");
+      sfpConnect(&alice);
+    }
+#endif
+}
---
+++ tests/simulatedmedium.hpp
@@ -0,0 +1,117 @@
+#ifndef SIMULATEDMEDIUM_HPP
+#define SIMULATEDMEDIUM_HPP
+
+#include <tbb/concurrent_queue.h>
+#include <boost/optional.hpp>
+#include <queue>
+#include <atomic>
+#include <thread>
+#include <cmath>
+
+/* Simulate a physical medium using three buffers (queues): a write buffer, an
+ * intermediate buffer, and a read buffer. The user puts as many bytes as s/he
+ * wants into the write buffer; a thread (started on object construction, shut
+ * down on object destruction) moves bytes from the write buffer to the
+ * intermediate buffer, and from the intermediate buffer to the read buffer, at
+ * a fixed time interval.
+ *
+ * Calling write and read from separate threads is safe. Writes and reads block
+ * until the request is satisfied. */
+class SimulatedMedium {
+    using Quantum = boost::optional<uint8_t>;
+    std::queue<Quantum> mMedium;
+
+    using MediumContainer = decltype(mMedium)::container_type;
+
+public:
+    using Milliseconds = std::chrono::duration<double, std::chrono::milliseconds::period>;
+
+    SimulatedMedium (Milliseconds propagationDelay, unsigned baud)
+            // Pack the medium full of propagation-delay's worth of nothing.
+            : mMedium(MediumContainer(capacity(propagationDelay, baud), boost::none))
+            , mBaud(baud)
+            , mThread(&SimulatedMedium::serviceThread, this) {
+        // Take advantage of concurrent_bounded_queue's blocking push feature
+        // to avoid spinlocking on write.
+        mWriteQueue.set_capacity(1);
+    }
+
+    ~SimulatedMedium () {
+        mKillThread = true;
+        mThread.join();
+    }
+
+    // Block trying to read a byte.
+    uint8_t read () {
+        uint8_t octet;
+        mReadQueue.pop(octet);
+        return octet;
+    }
+
+    // Block trying to write a byte.
+    void write (uint8_t octet) {
+        // Since we set the capacity of the write buffer to one, this will
+        // block if something's already in there.
+        mWriteQueue.push(octet);
+    }
+
+    void write (std::string s) {
+        for (auto c : s) {
+            write(static_cast<uint8_t>(c));
+        }
+    }
+
+private:
+    Milliseconds tickInterval () const {
+        // Convert bits-per-second to milliseconds-per-byte.
+        return Milliseconds { 1000.0 / (double(mBaud) / 8) };
+    }
+
+    // Calculate how many bytes will fit in our intermediate buffer based on
+    // the baud rate and propagation delay.
+    static unsigned capacity (Milliseconds propagationDelay, unsigned baud) {
+        return std::ceil(double(baud / 8) * propagationDelay / Milliseconds(1000));
+    }
+
+    void serviceThread () {
+        while (!mKillThread) {
+            auto nextTimePoint = std::chrono::steady_clock::now() + tickInterval();
+
+            // If an octet is available from the write buffer, load it into the
+            // intermediate buffer. Otherwise, push nothing to keep the pipe
+            // flowing.
+            uint8_t octet;
+            mMedium.push(mWriteQueue.try_pop(octet)
+                    ? Quantum(octet)
+                    : Quantum(boost::none));
+#if 0
+            if (mWriteQueue.try_pop(octet)) {
+                mMedium.push(octet);
+            }
+            else {
+                mMedium.push(boost::none);
+            }
+#endif
+
+            auto quantum = mMedium.front();
+            mMedium.pop();
+            if (quantum) {
+                mReadQueue.push(*quantum);
+            }
+
+            assert(nextTimePoint > std::chrono::steady_clock::now() &&
+                    "Process is too slow to simulate this baud rate.");
+            std::this_thread::sleep_until(nextTimePoint);
+        }
+    }
+
+    const unsigned mBaud;
+
+    tbb::concurrent_bounded_queue<uint8_t> mWriteQueue;
+    tbb::concurrent_bounded_queue<uint8_t> mReadQueue;
+
+    std::atomic<bool> mKillThread = { false };
+    std::thread mThread;
+};
+
+#endif
