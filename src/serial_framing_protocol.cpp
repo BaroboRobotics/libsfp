03ec72581b21039842743cf697eece6396dc5418
Harris Hancock <harris@barobo.com>
2016-08-09 09:36:43 -0700

    Add copyright line to license file, update date in oldest sfp source

--- src/serial_framing_protocol.cpp
+++ src/serial_framing_protocol.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2016 Barobo, Inc.
+// Copyright (c) 2013-2016 Barobo, Inc.
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
