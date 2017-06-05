c1611565738d270fad7806d06a11d75b067c6c6d
Harris Hancock <harris@barobo.com>
2017-06-05 11:27:43 -0700

    Build and bug fixes

--- include/sfp/stream.hpp
+++ include/sfp/stream.hpp
@@ -27,9 +27,8 @@
 
 namespace sfp {
 
-static const std::chrono::milliseconds kSfpConnectTimeout { 100 } ;
+static const std::chrono::milliseconds kSfpConnectTimeout { 500 } ;
 static const std::chrono::milliseconds kSfpSettleTimeout { 200 } ;
-static const int kSfpMaxHandshakeAttempts { 50 };
 
 template <class AsyncStream, class Alloc = std::allocator<char>>
 class stream {
@@ -40,9 +39,6 @@ class stream {
     beast::basic_multi_buffer<Alloc> read_buffer;
     beast::basic_multi_buffer<Alloc> write_buffer;
 
-    beast::consuming_buffers<decltype(read_buffer.data())> input_sequence;
-    // View into read_buffer, if there are bytes that have been read but not yet SFP-processed.
-
     SFPcontext sfp_context;
     SFPpacket sfp_packet;
 
@@ -52,8 +48,8 @@ public:
         : next_layer_(std::forward<Args>(args)...)
         , write_strand(next_layer_.get_io_service())
         , write_phaser(write_strand)
-        , read_buffer(alloc)
-        , write_buffer(alloc)
+        , read_buffer(1024, alloc)
+        , write_buffer(1024, alloc)
     {}
 
 #ifdef SFP_CONFIG_DEBUG
@@ -71,10 +67,10 @@ private:
     template <class Handler = void(boost::system::error_code)>
     struct handshake_op;
 
-    template <class DynamicBuffer, class Handler = void(boost::system::error_code, size_t)>
+    template <class DynamicBuffer, class Handler = void(boost::system::error_code)>
     struct read_op;
 
-    template <class ConstBufferSequence, class Handler = void(boost::system::error_code)>
+    template <class Handler = void(boost::system::error_code)>
     struct write_op;
 
 public:
@@ -86,14 +82,14 @@ public:
     template <class DynamicBuffer, class Token>
     auto async_read(DynamicBuffer& buffer, Token&& token) {
         // Read one message into the provided buffer.
-        BOOST_ASSERT(sfpIsConnected(&self.sfp_context), "SFP handshake not yet completed");
+        BOOST_ASSERT_MSG(sfpIsConnected(&sfp_context), "SFP handshake not yet completed");
         return composed::operation<read_op<DynamicBuffer>>{}(*this, buffer, std::forward<Token>(token));
     }
 
-    template <class ConstBufferSequence, class Token>
-    auto async_write(const ConstBufferSequence& buffer, Token&& token) {
-        BOOST_ASSERT(sfpIsConnected(&self.sfp_context), "SFP handshake not yet completed");
-        return composed::operation<write_op<ConstBufferSequence>>{}(*this, buffer, std::forward<Token>(token));
+    template <class Token>
+    auto async_write(const boost::asio::const_buffer& buffer, Token&& token) {
+        BOOST_ASSERT_MSG(sfpIsConnected(&sfp_context), "SFP handshake not yet completed");
+        return composed::operation<write_op<>>{}(*this, buffer, std::forward<Token>(token));
     }
 
 private:
@@ -122,18 +118,26 @@ private:
     }
 
     void deliver_input_sequence() {
+        using boost::asio::buffers_begin;
+        using boost::asio::buffers_end;
         sfp_packet.len = 0;
-        for (auto octet: input_sequence) {
+        size_t delivered = 0;
+        auto buffer = read_buffer.data();
+        for (auto octet = buffers_begin(buffer); octet != buffers_end(buffer); ++octet) {
             auto& pkt = sfp_packet;
-            auto rc = sfpDeliverOctet(
-                    &sfp_context, octet, pkt.buf, sizeof(pkt.buf), &pkt.len);
+            auto rc = sfpDeliverOctet(&sfp_context, *octet, pkt.buf, sizeof(pkt.buf), &pkt.len);
+            ++delivered;
             BOOST_ASSERT(rc != -1);
+            // Would fail only if our output buffer, pkt, were smaller than SFP's internal packet
+            // data structure. But we're *using* SFP's internal packet data structure.
+
             // Ignore zero-length messages. This means we can rely on sfp_packet.len being non-zero
             // to indicate the presence of a message.
             if (rc == 1 && pkt.len != 0) {
                 break;
             }
         }
+        read_buffer.consume(delivered);
     }
 };
 
@@ -143,9 +147,6 @@ struct stream<AsyncStream, Alloc>::handshake_op: boost::asio::coroutine {
     using handler_type = Handler;
     using allocator_type = beast::handler_alloc<char, handler_type>;
     using executor_type = composed::handler_executor<handler_type>;
-
-    using logger_type = composed::logger;
-    logger_type get_logger() const { return &lg; }
 
     stream& self;
 
@@ -172,12 +173,13 @@ operator()(composed::op<handshake_op>& op) {
                 static_cast<stream*>(data)->on_sfp_write(octets, len, outlen);
             }, &self);
         sfpConnect(&self.sfp_context);
-        yield return boost::asio::async_write(self.next_layer_, self.write_buffer.data(), op(ec, std::ignore));
+        yield return boost::asio::async_write(self.next_layer_, self.write_buffer.data(),
+                composed::timed(self.next_layer_, kSfpConnectTimeout, op(ec, std::ignore)));
         self.write_buffer.consume(self.write_buffer.size());
 
         if (!sfpIsConnected(&self.sfp_context)) {
             yield return self.async_read_until_connected(composed::timed(
-                    self.next_layer_, kSfpConnectTimeout, op(ec));
+                    self.next_layer_, kSfpConnectTimeout, op(ec)));
         }
 
         // If async_read_until_connected() ended with us receiving a message (non-zero
@@ -188,19 +190,13 @@ operator()(composed::op<handshake_op>& op) {
 
         if (self.sfp_packet.len == 0) {
             yield return self.async_read_until_delivery(composed::timed(
-                    self.next_layer_, kSfpSettleTimeout, op(settle_ec));
+                    self.next_layer_, kSfpSettleTimeout, op(settle_ec)));
             ec = settle_ec == boost::asio::error::timed_out && sfpIsConnected(&self.sfp_context)
                     ? boost::system::error_code{}
                     : settle_ec;
             BOOST_ASSERT(sfpIsConnected(&self.sfp_context) || ec);
             // This assertion says: async_read_until_delivery cannot have completed successfully
             // without leaving the SFP context in a connected state.
-
-            BOOST_LOG(lg) << "SFP handshake settle period error: " << ec.message();
-        }
-
-        if (!ec) {
-            BOOST_LOG(lg) << "SFP handshake complete";
         }
     }
     op.complete(ec);
@@ -212,9 +208,6 @@ struct stream<AsyncStream, Alloc>::read_until_connected_op: boost::asio::corouti
     using handler_type = Handler;
     using allocator_type = beast::handler_alloc<char, handler_type>;
     using executor_type = composed::handler_executor<handler_type>;
-
-    using logger_type = composed::logger;
-    logger_type get_logger() const { return &lg; }
 
     stream& self;
     size_t rx_n = 0;
@@ -239,9 +232,9 @@ operator()(composed::op<read_until_connected_op>& op) {
             self.read_buffer.consume(self.read_buffer.size());
             yield return self.next_layer_.async_read_some(self.read_buffer.prepare(256), op(ec, rx_n));
             self.read_buffer.commit(rx_n);
-            self.input_sequence = {self.read_buffer};
             self.deliver_input_sequence();
-            yield return boost::asio::async_write(self.next_layer_, self.write_buffer.data(), op(ec, std::ignore));
+            yield return boost::asio::async_write(self.next_layer_, self.write_buffer.data(),
+                    composed::timed(self.next_layer_, kSfpConnectTimeout, op(ec, std::ignore)));
             self.write_buffer.consume(self.write_buffer.size());
         }
     }
@@ -254,9 +247,6 @@ struct stream<AsyncStream, Alloc>::read_until_delivery_op: boost::asio::coroutin
     using handler_type = Handler;
     using allocator_type = beast::handler_alloc<char, handler_type>;
     using executor_type = composed::handler_executor<handler_type>;
-
-    using logger_type = composed::logger;
-    logger_type get_logger() const { return &lg; }
 
     stream& self;
     size_t rx_n = 0;
@@ -278,14 +268,14 @@ void stream<AsyncStream, Alloc>::read_until_delivery_op<Handler>::
 operator()(composed::op<read_until_delivery_op>& op) {
     if (!ec) reenter(this) {
         BOOST_ASSERT(self.sfp_packet.len == 0);
-        BOOST_ASSERT(self.input_sequence.begin() == self.input_sequence.end());
+        BOOST_ASSERT(self.read_buffer.size() == 0);
         while (self.sfp_packet.len == 0) {
             self.read_buffer.consume(self.read_buffer.size());
             yield return self.next_layer_.async_read_some(self.read_buffer.prepare(256), op(ec, rx_n));
             self.read_buffer.commit(rx_n);
-            self.input_sequence = {self.read_buffer};
             self.deliver_input_sequence();
-            yield return boost::asio::async_write(self.next_layer_, self.write_buffer.data(), op(ec, std::ignore));
+            yield return boost::asio::async_write(self.next_layer_, self.write_buffer.data(),
+                    composed::timed(self.next_layer_, kSfpConnectTimeout, op(ec, std::ignore)));
             self.write_buffer.consume(self.write_buffer.size());
         }
     }
@@ -325,8 +315,11 @@ template <class DynamicBuffer, class Handler>
 void stream<AsyncStream, Alloc>::read_op<DynamicBuffer, Handler>::
 operator()(composed::op<read_op>& op) {
     if (!ec) reenter(this) {
-        if (self.sfp_packet.len == 0 && self.input_sequence.begin() != self.input_sequence.end()) {
-            // Drain the input_sequence buffers if there's anything left in there.
+        BOOST_LOG(lg) << "Starting read_op";
+        if (self.sfp_packet.len == 0 && self.read_buffer.size()) {
+            BOOST_LOG(lg) << "Draining read_buffer";
+
+            // Drain the read_buffer if there's anything left in there.
             yield return self.write_phaser.dispatch(op());
             work = composed::make_work_guard(self.write_phaser);
             self.deliver_input_sequence();
@@ -337,10 +330,11 @@ operator()(composed::op<read_op>& op) {
         }
 
         while (!flush_ec && self.sfp_packet.len == 0) {
+            BOOST_LOG(lg) << "reading and writing";
+
             self.read_buffer.consume(self.read_buffer.size());
             yield return self.next_layer_.async_read_some(self.read_buffer.prepare(256), op(ec, rx_n));
             self.read_buffer.commit(rx_n);
-            self.input_sequence = {self.read_buffer};
             yield return self.write_phaser.dispatch(op());
             work = composed::make_work_guard(self.write_phaser);
             self.deliver_input_sequence();
@@ -349,6 +343,8 @@ operator()(composed::op<read_op>& op) {
             self.write_buffer.consume(self.write_buffer.size());
             work = {};
         }
+
+        BOOST_LOG(lg) << "read_op completing with packet size " << self.sfp_packet.len;
 
         rx_n = boost::asio::buffer_copy(
                 dest_buffer.prepare(self.sfp_packet.len),
@@ -363,11 +359,11 @@ operator()(composed::op<read_op>& op) {
             // Defer propagating any flush error 
         }
     }
-    op.complete(ec, rx_n);
+    op.complete(ec);
 }
 
 template <class AsyncStream, class Alloc>
-template <class ConstBufferSequence, class Handler>
+template <class Handler>
 struct stream<AsyncStream, Alloc>::write_op: boost::asio::coroutine {
     using handler_type = Handler;
     using allocator_type = beast::handler_alloc<char, handler_type>;
@@ -377,13 +373,13 @@ struct stream<AsyncStream, Alloc>::write_op: boost::asio::coroutine {
     logger_type get_logger() const { return &lg; }
 
     stream& self;
-    ConstBufferSequence& source_buffer;
+    boost::asio::const_buffer source_buffer;
     composed::work_guard<composed::phaser<boost::asio::io_service::strand&>> work;
 
     composed::associated_logger_t<handler_type> lg;
     boost::system::error_code ec;
 
-    write_op(handler_type& h, stream& s, const ConstBufferSequence& b)
+    write_op(handler_type& h, stream& s, const boost::asio::const_buffer& b)
         : self(s)
         , source_buffer(b)
         , lg(composed::get_associated_logger(h))
@@ -393,8 +389,8 @@ struct stream<AsyncStream, Alloc>::write_op: boost::asio::coroutine {
 };
 
 template <class AsyncStream, class Alloc>
-template <class ConstBufferSequence, class Handler>
-void stream<AsyncStream, Alloc>::write_op<ConstBufferSequence, Handler>::
+template <class Handler>
+void stream<AsyncStream, Alloc>::write_op<Handler>::
 operator()(composed::op<write_op>& op) {
     if (!ec) reenter(this) {
         yield return self.write_phaser.dispatch(op());
